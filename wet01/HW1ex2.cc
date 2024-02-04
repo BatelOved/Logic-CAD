e61d0f9acaed67b2c6c6c99743d8e6487cc688f5
BatelOved <bateloved1@gmail.com>
2024-02-05 00:32:03 +0200

    HW1ex2 - in progress

--- wet01/HW1ex2.cc
+++ wet01/HW1ex2.cc
@@ -4,11 +4,149 @@
 #include <fstream>
 #include "hcm.h"
 #include "flat.h"
+#include <list>
 
 using namespace std;
 
 bool verbose = false;
 
+// ./gl_rank TopLevel1355 stdcell.v c1355high.v
+// ./gl_rank TopLevel2670 stdcell.v c2670high.v
+
+string getHierNamePropVal(hcmInstance* cell) {
+  // TODO
+  return "";
+}
+
+list<int> getRankInputListInitPropVal(hcmInstance* cell) {
+  return list<int>();
+}
+
+int getRankInitPropVal(hcmInstance* cell) {
+  return 0;
+}
+
+template <typename T>
+void setPropTree(hcmCell* cell, string PropName, T (*getPropVal)(hcmInstance*)) {
+
+  std::map<std::string, hcmInstance*>::const_iterator iI;
+
+  for (iI = cell->getInstances().begin(); iI != cell->getInstances().end(); iI++) {
+    assert(iI->second->setProp<T>(PropName, getPropVal(iI->second)) == OK);
+    hcmCell* currCell = iI->second->masterCell();
+    setPropTree<T>(currCell, PropName, getPropVal);
+  }
+}
+
+template <typename T>
+void delPropTree(hcmCell* cell, string PropName) {
+
+  std::map<std::string, hcmInstance*>::const_iterator iI;
+
+  for (iI = cell->getInstances().begin(); iI != cell->getInstances().end(); iI++) {
+    assert(iI->second->delProp<T>(PropName) == OK);
+    hcmCell* currCell = iI->second->masterCell();
+    delPropTree<T>(currCell, PropName);
+  }
+}
+
+void traverseNode(list<pair<int, hcmNode*>>* nodesList, pair<int, hcmNode*> nodePair, string rankPropName, string rankInputListPropName, string hierPropName) {
+  list<pair<int, hcmNode*>> outputNodePairs;
+  std::map<std::string, hcmInstPort*>::const_iterator ipI;
+
+  list<pair<int, hcmNode*>>& nodesListtmp = *nodesList; // tmp
+
+  for (ipI = nodePair.second->getInstPorts().begin(); ipI != nodePair.second->getInstPorts().end(); ipI++) {
+    hcmInstance* currInst = ipI->second->getInst();
+    hcmCell* currCell = currInst->masterCell();
+    hcmNode* currNode = currCell->getNode(ipI->second->getPort()->getName());
+
+    // if (globalNodes->find(currNode->getName()) != globalNodes->end()) {
+    //   continue;
+    // }
+
+    if (currCell->getInstances().size() == 0) {
+      list<int> rankInputList;
+      
+      assert(currInst->getProp<list<int>>(rankInputListPropName, rankInputList) == OK);
+      rankInputList.push_back(nodePair.first); // To Check
+
+      assert(currInst->setProp<list<int>>(rankInputListPropName, rankInputList) == OK);
+      
+
+      vector<hcmPort*> cellPorts = currCell->getPorts();
+
+      size_t inputSize = 0;
+
+      for (auto port : cellPorts) { // Change to prop
+        inputSize += port->getDirection() == IN; // IN_OUT?
+      }
+
+      if (rankInputList.size() == inputSize) {
+        int maxRank = 0, currRank = 0;
+        for (auto it : rankInputList) {
+          currRank = it;
+          maxRank = max(maxRank, currRank);
+        }
+        assert(currInst->setProp<int>(rankPropName, maxRank) == OK);
+        cout << "Cell Name: " << currInst->getName() << " Rank: " << maxRank << endl;
+
+        for (auto port : cellPorts) { // Change to prop
+          if (port->getDirection() == OUT) {
+            outputNodePairs.push_front(pair<int, hcmNode*>(maxRank+1, port->owner()));
+          }
+        }
+
+        for (auto it : outputNodePairs) {
+          nodesList->push_back(it);
+        }
+      }
+
+      continue;
+    }
+
+    traverseNode(nodesList, pair<int, hcmNode*>(nodePair.first, currNode), rankPropName, rankInputListPropName, hierPropName);
+  }
+}
+
+vector<pair<int, string>> getMaxRankVectorByProp(hcmCell* cell, list<pair<int, hcmNode*>>* nodesList, string rankPropName, string rankInputListPropName, string hierPropName) {
+  vector<pair<int, string>> maxRankVector;
+
+  list<pair<int, hcmNode*>>& nodesListtmp = *nodesList; // tmp
+  
+  while(!(nodesList->empty())) {
+    pair<int, hcmNode*> currNodePair = nodesList->front();
+    nodesList->pop_front();
+
+    traverseNode(nodesList, currNodePair, rankPropName, rankInputListPropName, hierPropName);
+  }
+
+  return maxRankVector; // TODO
+}
+
+vector<pair<int, string>> getMaxRankVector(hcmCell* cell) {
+  string rankPropName = "rank";
+  string rankInputListPropName = "rankInputList";
+  string hierPropName = "hierName";
+  list<pair<int, hcmNode*>> nodesList;
+  setPropTree<int>(cell, rankPropName, getRankInitPropVal);
+  setPropTree<list<int>>(cell, rankInputListPropName, getRankInputListInitPropVal);
+  setPropTree<string>(cell, hierPropName, getHierNamePropVal);
+
+  for (auto pI : cell->getPorts()) {
+    if (pI->getDirection() == IN) {
+      nodesList.push_back(pair<int, hcmNode*>(0, pI->owner()));
+    }
+  }
+
+  vector<pair<int, string>> maxRankVector = getMaxRankVectorByProp(cell, &nodesList, rankPropName, rankInputListPropName, hierPropName);
+
+  delPropTree<int>(cell, rankPropName);
+  delPropTree<list<int>>(cell, rankInputListPropName);
+  delPropTree<string>(cell, hierPropName);
+
+  return maxRankVector;
+}
 
 int main(int argc, char **argv) {
   int argIdx = 1;
@@ -78,7 +216,7 @@ int main(int argc, char **argv) {
   */
   vector<pair<int, string>> maxRankVector;
   //---------------------------------------------------------------------------------//
-  //enter your code here 
+  maxRankVector = getMaxRankVector(topCell);
   //---------------------------------------------------------------------------------//
   for(auto itr = maxRankVector.begin(); itr != maxRankVector.end(); itr++){
 		fv << itr->first << " " << itr->second << endl;
